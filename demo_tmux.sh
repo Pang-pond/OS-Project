3a6d27685bf7e787392aebd078e05e2524b892fd
NxwYear <s.suphakom@gmail.com>
2025-11-05 03:29:37 +0700

    Update demo_tmux.sh

--- demo_tmux.sh
+++ demo_tmux.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 SESSION="ipc_chat_demo"
-cd /app
 tmux new-session -d -s "$SESSION" './server'
 tmux split-window -h -t "$SESSION":0 './client 1001 Alice'
 tmux split-window -v -t "$SESSION":0.1 './client 1002 Bob'
